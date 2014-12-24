b8fee3d45c101e075449e1dacf3ecc099da4e426
leafriend <leafriend@gmail.com>
2014-12-24 15:01:04 +0900

    Clean up codes as test case

--- src/test/java/net/folab/fo/runtime/_fo/_lang/BooleanTest.java
+++ src/test/java/net/folab/fo/runtime/_fo/_lang/BooleanTest.java
@@ -2,9 +2,10 @@ package net.folab.fo.runtime._fo._lang;
 
 import static net.folab.fo.runtime._fo._lang.Boolean.*;
 import static net.folab.fo.runtime._fo._lang.Integer.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
-import net.folab.fo.runtime.Evaluable;
-import net.folab.fo.runtime.Function;
+import net.folab.fo.runtime._fo._lang._Boolean.choose;
 
 import org.junit.After;
 import org.junit.Before;
@@ -21,32 +22,27 @@ public class BooleanTest {
     }
 
     @Test
-    public void test() {
+    public void test_Boolean() {
 
-        Function<Evaluable<Boolean>, Function<Evaluable<Boolean>, Evaluable<Boolean>>> falseChoose = FALSE
-                .choose();
-        Function<Evaluable<Boolean>, Function<Evaluable<Boolean>, Evaluable<Boolean>>> trueChoose = TRUE
-                .choose();
+        choose<Boolean> f = FALSE.choose();
+        choose<Boolean> t = TRUE.choose();
 
-        System.out.println(falseChoose.apply(FALSE).apply(TRUE).evaluate()
-                .getValue());
-        System.out.println(trueChoose.apply(FALSE).apply(TRUE).evaluate()
-                .getValue());
+        assertTrue(f.apply(FALSE).apply(TRUE).evaluate().getValue());
+        assertFalse(t.apply(FALSE).apply(TRUE).evaluate().getValue());
 
     }
 
     @Test
-    public void testI() {
+    public void test_Integer() {
 
-        Function<Evaluable<Integer>, Function<Evaluable<Integer>, Evaluable<Integer>>> falseChoose = FALSE
-                .choose();
-        Function<Evaluable<Integer>, Function<Evaluable<Integer>, Evaluable<Integer>>> trueChoose = TRUE
-                .choose();
+        choose<Integer> f = FALSE.choose();
+        choose<Integer> t = TRUE.choose();
 
-        System.out.println(falseChoose.apply(valueOf(1)).apply(valueOf(0))
-                .evaluate().getValue());
-        System.out.println(trueChoose.apply(valueOf(1)).apply(valueOf(0))
-                .evaluate().getValue());
+        Integer one = valueOf(1);
+        Integer zero = valueOf(0);
+
+        assertThat(f.apply(one).apply(zero).evaluate().getValue(), is(0));
+        assertThat(t.apply(one).apply(zero).evaluate().getValue(), is(1));
 
     }
 
