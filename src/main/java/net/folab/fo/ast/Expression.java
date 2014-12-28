8d38a9f33a8b7926a36ef053ff86e49837ad353a
leafriend <leafriend@gmail.com>
2014-12-28 17:49:21 +0900

    Add Expression

---
+++ src/main/java/net/folab/fo/ast/Expression.java
@@ -0,0 +1,26 @@
+package net.folab.fo.ast;
+
+import net.folab.fo.bytecode.JavaType;
+import net.folab.fo.bytecode.StatementContext;
+
+import org.objectweb.asm.MethodVisitor;
+
+public interface Expression {
+
+    public static final Expression VOID = new Expression() {
+
+        public JavaType getType() {
+            return JavaType.VOID;
+        }
+
+        public void generate(MethodVisitor mv, StatementContext context) {
+
+        }
+
+    };
+
+    public void generate(MethodVisitor mv, StatementContext context);
+
+    public JavaType getType();
+
+}
