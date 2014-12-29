890c5b32bad2078e4207f8d3b316d50f73e62be6
leafriend <leafriend@gmail.com>
2014-12-29 20:39:12 +0900

    Introduce MemberDeclaration to abtract decls.

--- src/main/java/net/folab/fo/ast/FunctionDeclaration.java
+++ src/main/java/net/folab/fo/ast/FunctionDeclaration.java
@@ -8,31 +8,26 @@ import net.folab.fo.bytecode.JavaType;
 import net.folab.fo.jast.Block;
 import net.folab.fo.jast.Statement;
 
-public class FunctionDeclaration {
-
-    public final Access access;
+public class FunctionDeclaration extends MemberDeclaration {
 
     public final JavaType returnType;
-
-    public final String name;
 
     public final JavaType[] parameterTypes;
 
     public final Block block;
 
-    public FunctionDeclaration(Access access, JavaType returnType,
-            String name, JavaType[] parameterTypes, boolean isStatic,
-            List<Statement> statements) {
-        this.access = access;
+    public FunctionDeclaration(Access access, JavaType type, String name,
+            JavaType returnType, JavaType[] parameterTypes,
+            boolean isStatic, List<Statement> statements) {
+        super(access, type, name);
         this.returnType = returnType;
-        this.name = name;
         this.parameterTypes = parameterTypes;
         this.block = new Block(isStatic, statements);
     }
 
     public static FunctionDeclaration build(String name) {
         return new FunctionDeclaration(Access.PUBLIC, JavaType.VOID, name,
-                new JavaType[0], false, new ArrayList<Statement>());
+                JavaType.VOID, new JavaType[0], false, new ArrayList<Statement>());
     }
 
     public void accept(AstVisitor cv) {
@@ -40,30 +35,30 @@ public class FunctionDeclaration {
     }
 
     public FunctionDeclaration setClassGenerator(ClassGenerator cg) {
-        return new FunctionDeclaration(access, returnType, name, parameterTypes,
-                block.isStatic, block.statements);
+        return new FunctionDeclaration(access, type, name, returnType,
+                parameterTypes, block.isStatic, block.statements);
     }
 
     public FunctionDeclaration setAccess(Access access) {
-        return new FunctionDeclaration(access, returnType, name, parameterTypes,
-                block.isStatic, block.statements);
+        return new FunctionDeclaration(access, type, name, returnType,
+                parameterTypes, block.isStatic, block.statements);
     }
 
     public FunctionDeclaration setReturnType(JavaType returnType) {
-        return new FunctionDeclaration(access, returnType, name, parameterTypes,
-                block.isStatic, block.statements);
+        return new FunctionDeclaration(access, returnType, name,
+                returnType, parameterTypes, block.isStatic, block.statements);
     }
 
     public FunctionDeclaration setParameterTypes(JavaType... parameterTypes) {
-        return new FunctionDeclaration(access, returnType, name, parameterTypes,
-                block.isStatic, block.statements);
+        return new FunctionDeclaration(access, type, name, returnType,
+                parameterTypes, block.isStatic, block.statements);
     }
 
     public FunctionDeclaration addStatement(Statement statement) {
         List<Statement> statements = new ArrayList<Statement>(block.statements);
         statements.add(statement);
-        return new FunctionDeclaration(access, returnType, name, parameterTypes,
-                block.isStatic, statements);
+        return new FunctionDeclaration(access, type, name, returnType,
+                parameterTypes, block.isStatic, statements);
     }
 
 }
---
+++ src/main/java/net/folab/fo/ast/MemberDeclaration.java
@@ -0,0 +1,20 @@
+package net.folab.fo.ast;
+
+import net.folab.fo.bytecode.Access;
+import net.folab.fo.bytecode.JavaType;
+
+public class MemberDeclaration {
+
+    public final Access access;
+
+    public final JavaType type;
+
+    public final String name;
+
+    public MemberDeclaration(Access access, JavaType type, String name) {
+        this.access = access;
+        this.type = type;
+        this.name = name;
+    }
+
+}
